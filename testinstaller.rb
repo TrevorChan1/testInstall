1d0079441bf6fe0cf323b605205ee7ba32ac4f1f
Trevor Chan <Trevor.chan@pemicro.com>
2022-06-23 16:34:57 -0400

    Added files

---
+++ testinstaller.rb
@@ -0,0 +1,40 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Testinstaller < Formula
+  desc "Test installer for cycloneControlSDK_python"
+  homepage "https://www.pemicro.com/"
+  url "https://github.com/TrevorChan1/testInstall/raw/main/testInstaller.tar"
+  version "1.0.0"
+  sha256 "8e917466db99d77d3300eafa811d8052a679d79869e3ae72164431c350418492"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test testInstaller`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    return if OS.linux?
+    system "#{bin}/testInstaller", "--help"
+  end
+end
